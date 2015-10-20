e82ac7c2b8de3b7c0928543c9dacc1099dcfe39e
cristiingineru <cristi.ingineru@gmail.com>
2015-10-20 21:20:01 +0300

    Tests

---
+++ ExpressionViewerTests/ExpressionSearcherTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Extension;
+
+namespace ExpressionViewerTests
+{
+    [TestClass]
+    public class ExpressionSearcherTests
+    {
+        [TestMethod]
+        public void ExpressionSearcher_Constructor()
+        {
+            var searcher = new ExpressionSearcher();
+        }
+
+        [TestMethod]
+        public void ExpressionSearcher_WithNullSolution_ReturnsNoValue()
+        {
+            var searcher = new ExpressionSearcher();
+
+            var result = searcher.FindTarget(null);
+
+            Assert.IsFalse(result.HasValue);
+        }
+    }
+}
---
+++ Extension/ExpressionSearcher.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public class ExpressionSearcher
+    {
+        public Nullable<int> FindTarget(Solution solution)
+        {
+            return null;
+        }
+    }
+}
