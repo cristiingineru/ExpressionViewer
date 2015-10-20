bfad7837f1aa5ca9776cfdb16c8d068a82be99ab
cristiingineru <cristi.ingineru@gmail.com>
2015-10-20 23:11:17 +0300

    New Tests

--- ExpressionViewerTests/ExpressionSearcherTests.cs
+++ ExpressionViewerTests/ExpressionSearcherTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Extension;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Threading.Tasks;
 
 namespace ExpressionViewerTests
 {
@@ -8,19 +11,84 @@ namespace ExpressionViewerTests
     public class ExpressionSearcherTests
     {
         [TestMethod]
-        public void ExpressionSearcher_Constructor()
+        public async Task ExpressionSearcher_WithNullSolution_ReturnsNoValue()
         {
             var searcher = new ExpressionSearcher();
+
+            var result = await searcher.FindTarget(null);
+
+            Assert.IsFalse(result.HasValue);
         }
 
         [TestMethod]
-        public void ExpressionSearcher_WithNullSolution_ReturnsNoValue()
+        public async Task ExpressionSearcher_WithEmptySolution_ReturnsNoValue()
         {
             var searcher = new ExpressionSearcher();
 
-            var result = searcher.FindTarget(null);
+            var solution = EmptySolution();
+            var result = await searcher.FindTarget(solution);
 
             Assert.IsFalse(result.HasValue);
+        }
+
+        [TestMethod]
+        public async Task ExpressionSearcher_WithSolutionWithSingleMethod_ReturnsTheMethod()
+        {
+            var searcher = new ExpressionSearcher();
+
+            var solution = SolutionWithSingleMethod();
+            var result = await searcher.FindTarget(solution);
+
+            Assert.IsTrue(result.HasValue);
+        }
+
+
+        private Solution EmptySolution()
+        {
+            var id = SolutionId.CreateNewId();
+            var version = new VersionStamp();
+            var solutionInfo = SolutionInfo.Create(id, version);
+            var workspace = new AdhocWorkspace();
+            var solution = workspace.AddSolution(solutionInfo);
+
+            return solution;
+        }
+
+        private Solution SolutionWithSingleMethod()
+        {
+            var id = SolutionId.CreateNewId();
+            var version = new VersionStamp();
+            var solutionInfo = SolutionInfo.Create(id, version);
+            var workspace = new AdhocWorkspace();
+            var solution = workspace.AddSolution(solutionInfo);
+
+            var root = CSharpSyntaxTree.ParseText(@"
+                public class MyClass
+                {
+                    public void MyMethod()
+                    {
+                    }
+                }").GetRoot();
+
+
+            var projectId = ProjectId.CreateNewId();
+            var projectInfo = NewProjectInfo(projectId);
+            solution = solution.AddProject(projectInfo);
+            var project = solution.GetProject(projectId);
+            var document = project.AddDocument("file.cs", root);
+
+            return document.Project.Solution;
+
+            //return solution;
+        }
+
+        private ProjectInfo NewProjectInfo(ProjectId projectId)
+        {
+            //var projectId = ProjectId.CreateNewId();
+            var version = VersionStamp.Create();
+            var projectInfo = ProjectInfo.Create(projectId, version, "no name", "assembly.dll", "C#");
+
+            return projectInfo;
         }
     }
 }
--- Extension/ExpressionSearcher.cs
+++ Extension/ExpressionSearcher.cs
@@ -9,8 +9,22 @@ namespace Extension
 {
     public class ExpressionSearcher
     {
-        public Nullable<int> FindTarget(Solution solution)
+        public async Task<Nullable<int>> FindTarget(Solution solution)
         {
+            if (solution == null)
+            {
+                return null;
+            }
+
+            var projectIds = solution.GetProjectDependencyGraph().GetTopologicallySortedProjects();
+
+            var projects = projectIds.Select(projectId => solution.GetProject(projectId));
+
+            var compilation = (await projects.First().GetCompilationAsync());
+            //t.Wait();
+            //var compilation = t.Result;
+            var roots = compilation.SyntaxTrees.Select(syntaxTree => syntaxTree.GetRoot());
+
             return null;
         }
     }
