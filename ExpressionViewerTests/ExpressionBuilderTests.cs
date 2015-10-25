e6615016901454d0fb7730dd231dd12a91454e19
cristiingineru <cristi.ingineru@gmail.com>
2015-10-25 12:49:00 +0200

    ExpressionBuilder

---
+++ ExpressionViewerTests/ExpressionBuilderTests.cs
@@ -0,0 +1,24 @@
+﻿using Extension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionViewerTests
+{
+    [TestClass]
+    public class ExpressionBuilderTests
+    {
+        [TestMethod]
+        public async Task BuildWrapper_WithNullInnerExpression_ReturnsWrapper()
+        {
+            var builder = new ExpressionBuilder();
+
+            var result = await builder.BuildWrapper(null);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ Extension/ExpressionBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public class ExpressionBuilder
+    {
+        public async Task<SyntaxNode> BuildWrapper(SyntaxNode innerExpression)
+        {
+            return null;
+        }
+    }
+}
