92bb794530abfb64270b9f05965c9b1abba9a61f
cristiingineru <cristi.ingineru@gmail.com>
2015-10-25 20:30:46 +0200

    TempDirectory

---
+++ ExpressionViewerTests/TempDirectoryTests.cs
@@ -0,0 +1,33 @@
+﻿using Extension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionViewerTests
+{
+    [TestClass]
+    public class TempDirectoryTests
+    {
+        [TestMethod]
+        public void TempDirectory_Constructor_ReturnsIDisposableObject()
+        {
+            var tempDirectory = new TempDirectory();
+
+            Assert.IsTrue(tempDirectory is IDisposable);
+        }
+
+        [TestMethod]
+        public void TempDirectory_Directory_ReturnsExistingDirectory()
+        {
+            var tempDirectory = new TempDirectory();
+
+            var directory = tempDirectory.Directory;
+
+            Assert.IsTrue(Directory.Exists(directory));
+        }
+    }
+}
---
+++ Extension/TempDirectory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public class TempDirectory : IDisposable
+    {
+        public void Dispose()
+        {
+            if (disposed == false)
+            {
+                disposed = true;
+            }
+        }
+
+        public TempDirectory()
+        {
+            Directory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            System.IO.Directory.CreateDirectory(Directory);
+        }
+
+        public string Directory { get; private set; }
+
+        private bool disposed = false;
+    }
+}
