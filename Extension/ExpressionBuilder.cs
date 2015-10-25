3b15589f26a5ac96ab195a6a58a6dab93d6f18d5
cristiingineru <cristi.ingineru@gmail.com>
2015-10-25 14:18:21 +0200

    Dummy BuildWrapper

--- Extension/ExpressionBuilder.cs
+++ Extension/ExpressionBuilder.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,54 @@ namespace Extension
     {
         public async Task<SyntaxNode> BuildWrapper(SyntaxNode innerExpression)
         {
-            return null;
+            var newClass =
+                SyntaxFactory.ClassDeclaration("MyClass")
+                .WithModifiers(
+                    SyntaxFactory.TokenList(
+                        SyntaxFactory.Token(
+                            SyntaxKind.PublicKeyword)))
+                .AddMembers(
+                    SyntaxFactory.MethodDeclaration(
+                        SyntaxFactory.PredefinedType(
+                            SyntaxFactory.Token(
+                                SyntaxKind.StringKeyword)),
+                        SyntaxFactory.Identifier(
+                            @"MyMethod"))
+                .WithModifiers(
+                    SyntaxFactory.TokenList(
+                        SyntaxFactory.Token(
+                            SyntaxKind.PublicKeyword),
+                        SyntaxFactory.Token(
+                            SyntaxKind.StaticKeyword)))
+                .WithParameterList(
+                    SyntaxFactory.ParameterList()
+                    .WithOpenParenToken(
+                        SyntaxFactory.Token(
+                            SyntaxKind.OpenParenToken))
+                    .WithCloseParenToken(
+                        SyntaxFactory.Token(
+                            SyntaxKind.CloseParenToken)))
+                .WithBody(
+                    SyntaxFactory.Block(
+                        SyntaxFactory.SingletonList<StatementSyntax>(
+                            SyntaxFactory.ReturnStatement(
+
+                                innerExpression as ExpressionSyntax)
+
+                            .WithReturnKeyword(
+                                SyntaxFactory.Token(
+                                    SyntaxKind.ReturnKeyword))
+                            .WithSemicolonToken(
+                                SyntaxFactory.Token(
+                                    SyntaxKind.SemicolonToken))))
+                    .WithOpenBraceToken(
+                        SyntaxFactory.Token(
+                            SyntaxKind.OpenBraceToken))
+                    .WithCloseBraceToken(
+                        SyntaxFactory.Token(
+                            SyntaxKind.CloseBraceToken))));
+
+            return newClass;
         }
     }
 }
