58f73214e3254ac11f25431b560a98623911ef40
cristiingineru <cristi.ingineru@gmail.com>
2015-10-27 22:59:31 +0200

    AssemblyRunner

---
+++ ExpressionViewerTests/AssemblyRunnerTests.cs
@@ -0,0 +1,24 @@
+﻿using Extension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionViewerTests
+{
+    [TestClass]
+    public class AssemblyRunnerTests
+    {
+        [TestMethod]
+        public void AssemblyRunner_WithNullPath_ReturnsNothing()
+        {
+            var runner = new AssemblyRunner();
+
+            var result = runner.Run(null);
+
+            Assert.AreEqual(String.Empty, result as string);
+        }
+    }
+}
---
+++ Extension/AssemblyRunner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public class AssemblyRunner
+    {
+        public object Run(string path)
+        {
+            return String.Empty;
+        }
+    }
+}
